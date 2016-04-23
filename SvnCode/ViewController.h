b36aff0af2c7e32f5bd22ba3045c7ec4bcffc9af
mac <mac@macdeMacBook-Air.local>
2016-04-23 12:41:22 +0800

    no message

---
+++ SvnCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SvnCode
+//
+//  Created by mac on 15/12/24.
+//  Copyright (c) 2015年 fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SvnCode/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  SvnCode
+//
+//  Created by mac on 15/12/24.
+//  Copyright (c) 2015年 fly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"第一次提交");
+    NSLog(@"第二次提交");
+    NSLog(@"第三次提交");
+    NSLog(@"第四次提交");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
